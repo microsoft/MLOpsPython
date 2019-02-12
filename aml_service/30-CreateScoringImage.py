6bb35693a29477a83d7b8430f7177bfc214df6f2
Richin Jain <rijai@microsoft.com>
2019-02-12 12:42:01 -0500

    build: Making use of CLIAuthentication class to fetch the workspace. This is a new change from AML SDK. Requires that you have ran az login on the machine before you run the scripts under aml_service folder

--- aml_service/30-CreateScoringImage.py
+++ aml_service/30-CreateScoringImage.py
@@ -27,9 +27,11 @@ import os, json, sys
 from azureml.core import Workspace
 from azureml.core.image import ContainerImage, Image
 from azureml.core.model import Model
+from azureml.core.authentication import AzureCliAuthentication
+cli_auth = AzureCliAuthentication()
 
 # Get workspace
-ws = Workspace.from_config()
+ws = Workspace.from_config(auth=cli_auth)
 
 # Get the latest model details
 
