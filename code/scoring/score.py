2bb53b586c08090fe63df6a5a1bb75392bb2fb17
Eugene Fedorenko <efedorenko@Eugenes-MBP.guest.corp.microsoft.com>
2019-08-09 16:55:56 -0700

    linting score

--- code/scoring/score.py
+++ code/scoring/score.py
@@ -34,7 +34,7 @@ def init():
 
     # load the model from file into a global object
     model_path = Model.get_model_path(
-        model_name="sklearn_regression_model.pkl")    
+        model_name="sklearn_regression_model.pkl")
     model = joblib.load(model_path)
 
 
