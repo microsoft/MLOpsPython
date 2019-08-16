fffe7c04d6f79b6d698bf3b215dc6ac77cb99917
Eugene Fedorenko <efedorenko@Eugenes-MBP.guest.corp.microsoft.com>
2019-08-16 16:56:28 -0700

    fixing auth error on invoking ML pipeline

--- ml_service/pipelines/run_train_pipeline.py
+++ ml_service/pipelines/run_train_pipeline.py
@@ -2,8 +2,12 @@ import sys
 import os
 import json
 import requests
-from azureml.core.authentication import AzureCliAuthentication
+from azure.common.credentials import ServicePrincipalCredentials
 
+
+tenant_id = os.environ.get("TENANT_ID")
+app_id = os.environ.get("SP_APP_ID")
+app_secret = os.environ.get("SP_APP_SECRET")
 
 try:
     with open("train_pipeline.json") as f:
@@ -15,16 +19,24 @@ except Exception:
 experiment_name = os.environ.get("EXPERIMENT_NAME")
 model_name = os.environ.get("MODEL_NAME")
 
-cli_auth = AzureCliAuthentication()
-token = cli_auth.get_authentication_header()
+credentials = ServicePrincipalCredentials(
+    client_id=app_id,
+    secret=app_secret,
+    tenant=tenant_id
+)
+
+token = credentials.token['access_token']
+print("token", token)
+auth_header = {"Authorization": "Bearer " + token}
 
 rest_endpoint = train_pipeline_json["rest_endpoint"]
 
 response = requests.post(
-    rest_endpoint, headers=token,
+    rest_endpoint, headers=auth_header,
     json={"ExperimentName": experiment_name,
           "ParameterAssignments": {"model_name": model_name}}
 )
 
 run_id = response.json()["Id"]
 print("Pipeline run initiated ", run_id)
+
