64bb1b0d0393122d5e06c4c5a2e5405b45030a7a
Eugene Fedorenko <efedorenko@Eugenes-MBP.guest.corp.microsoft.com>
2019-08-16 17:23:29 -0700

    cleaning

--- environment_setup/python.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $1 == 'run_train_pipeline.py' ]
-then
-  az login --service-principal -u $(SP_APP_ID) -p $(SP_APP_SECRET) --tenant $(TENANT_ID)
-  python3 $1
-else 
-  python3 $1
-fi
