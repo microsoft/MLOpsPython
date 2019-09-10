06d2324c073a3b681d52e58c30ed29de7559b218
Jordan Edwards <jordane@microsoft.com>
2019-09-10 15:28:37 -0700

    Update register_model.py
    Changing to use run.register_model

--- code/register/register_model.py
+++ code/register/register_model.py
@@ -27,7 +27,6 @@ import os
 import json
 import sys
 from azureml.core import Run
-from azureml.core.model import Model
 import argparse
 
 from azureml.core.authentication import AzureCliAuthentication
@@ -90,32 +89,16 @@ run = Run(experiment=exp, run_id=run_id)
 names = run.get_file_names
 names()
 print("Run ID for last run: {}".format(run_id))
-model_local_dir = "model"
-os.makedirs(model_local_dir, exist_ok=True)
 
-# Download Model to Project root directory
-# model_name = "sklearn_regression_model.pkl"
-run.download_file(
-    name="./outputs/" + model_name, output_file_path="./model/" + model_name
-)
-print("Downloaded model {} to Project root directory".format(model_name))
-os.chdir("./model")
-model = Model.register(
-    model_path=model_name,  # this points to a local file
-    model_name=model_name,  # this is the name the model is registered as
-    tags={"area": "diabetes", "type": "regression", "run_id": run_id},
-    description="Regression model for diabetes dataset",
-    workspace=ws,
-)
+model = run.register_model(model_name=model_name,
+                           model_path="./outputs/" + model_name,
+                           tags={"area": "diabetes", "type": "regression"})
 os.chdir("..")
 print(
     "Model registered: {} \nModel Description: {} \nModel Version: {}".format(
         model.name, model.description, model.version
     )
 )
-
-# Remove the evaluate.json as we no longer need it
-# os.remove("aml_config/evaluate.json")
 
 # Writing the registered model details to /aml_config/model.json
 model_json = {}
