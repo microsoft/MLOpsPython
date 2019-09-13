eceb936269350444f74538d175d99ed7ff56cba4
Taylor Rockey <tarockey@users.noreply.github.com>
2019-09-13 03:25:53 -0700

    Updated repo to use the SDK more consistently  (#67)
    * Updated code to use SDK instead of JSON files
    
    * include location in environment setup
    
    * docs updates
    
    * Update docker-image-pipeline.yml for Azure Pipelines
    
    * Update docker-image-pipeline.yml for Azure Pipelines

--- ml_service/pipelines/build_train_pipeline.py
+++ ml_service/pipelines/build_train_pipeline.py
@@ -1,16 +1,14 @@
 from azureml.pipeline.core.graph import PipelineParameter
 from azureml.pipeline.steps import PythonScriptStep
-from azureml.pipeline.core import Pipeline, PipelineData
+from azureml.pipeline.core import Pipeline  # , PipelineData
 from azureml.core.runconfig import RunConfiguration, CondaDependencies
-from azureml.core import Datastore
-import datetime
+# from azureml.core import Datastore
 import os
 import sys
 from dotenv import load_dotenv
 sys.path.append(os.path.abspath("./ml_service/util"))  # NOQA: E402
 from workspace import get_workspace
 from attach_compute import get_compute
-import json
 
 
 def main():
@@ -24,10 +22,14 @@ def main():
     sources_directory_train = os.environ.get("SOURCES_DIR_TRAIN")
     train_script_path = os.environ.get("TRAIN_SCRIPT_PATH")
     evaluate_script_path = os.environ.get("EVALUATE_SCRIPT_PATH")
-    register_script_path = os.environ.get("REGISTER_SCRIPT_PATH")
-    vm_size_cpu = os.environ.get("AML_COMPUTE_CLUSTER_CPU_SKU")
-    compute_name_cpu = os.environ.get("AML_COMPUTE_CLUSTER_NAME")
+    # register_script_path = os.environ.get("REGISTER_SCRIPT_PATH")
+    vm_size = os.environ.get("AML_COMPUTE_CLUSTER_CPU_SKU")
+    compute_name = os.environ.get("AML_COMPUTE_CLUSTER_NAME")
     model_name = os.environ.get("MODEL_NAME")
+    build_id = os.environ.get("BUILD_BUILDID")
+    pipeline_name = os.environ.get("TRAINING_PIPELINE_NAME")
+
+    print(app_secret)
 
     # Get Azure machine learning workspace
     aml_workspace = get_workspace(
@@ -40,12 +42,12 @@ def main():
     print(aml_workspace)
 
     # Get Azure machine learning cluster
-    aml_compute_cpu = get_compute(
+    aml_compute = get_compute(
         aml_workspace,
-        compute_name_cpu,
-        vm_size_cpu)
-    if aml_compute_cpu is not None:
-        print(aml_compute_cpu)
+        compute_name,
+        vm_size)
+    if aml_compute is not None:
+        print(aml_compute)
 
     run_config = RunConfiguration(conda_dependencies=CondaDependencies.create(
         conda_packages=['numpy', 'pandas',
@@ -58,23 +60,20 @@ def main():
 
     model_name = PipelineParameter(
         name="model_name", default_value=model_name)
-    def_blob_store = Datastore(aml_workspace, "workspaceblobstore")
-    jsonconfigs = PipelineData("jsonconfigs", datastore=def_blob_store)
-    config_suffix = datetime.datetime.now().strftime("%Y%m%d%H")
+    release_id = PipelineParameter(
+        name="release_id", default_value="0"
+    )
 
     train_step = PythonScriptStep(
         name="Train Model",
         script_name=train_script_path,
-        compute_target=aml_compute_cpu,
+        compute_target=aml_compute,
         source_directory=sources_directory_train,
         arguments=[
-            "--config_suffix", config_suffix,
-            "--json_config", jsonconfigs,
+            "--release_id", release_id,
             "--model_name", model_name,
         ],
         runconfig=run_config,
-        # inputs=[jsonconfigs],
-        outputs=[jsonconfigs],
         allow_reuse=False,
     )
     print("Step Train created")
@@ -82,52 +81,49 @@ def main():
     evaluate_step = PythonScriptStep(
         name="Evaluate Model ",
         script_name=evaluate_script_path,
-        compute_target=aml_compute_cpu,
+        compute_target=aml_compute,
         source_directory=sources_directory_train,
         arguments=[
-            "--config_suffix", config_suffix,
-            "--json_config", jsonconfigs,
+            "--release_id", release_id,
+            "--model_name", model_name,
         ],
         runconfig=run_config,
-        inputs=[jsonconfigs],
-        # outputs=[jsonconfigs],
         allow_reuse=False,
     )
     print("Step Evaluate created")
 
-    register_model_step = PythonScriptStep(
-        name="Register New Trained Model",
-        script_name=register_script_path,
-        compute_target=aml_compute_cpu,
-        source_directory=sources_directory_train,
-        arguments=[
-            "--config_suffix", config_suffix,
-            "--json_config", jsonconfigs,
-            "--model_name", model_name,
-        ],
-        runconfig=run_config,
-        inputs=[jsonconfigs],
-        # outputs=[jsonconfigs],
-        allow_reuse=False,
-    )
-    print("Step register model created")
+    # Currently, the Evaluate step will automatically register
+    # the model if it performs better. This step is based on a
+    # previous version of the repo which utilized JSON files to
+    # track evaluation results.
+
+    # register_model_step = PythonScriptStep(
+    #     name="Register New Trained Model",
+    #     script_name=register_script_path,
+    #     compute_target=aml_compute,
+    #     source_directory=sources_directory_train,
+    #     arguments=[
+    #         "--release_id", release_id,
+    #         "--model_name", model_name,
+    #     ],
+    #     runconfig=run_config,
+    #     allow_reuse=False,
+    # )
+    # print("Step register model created")
 
     evaluate_step.run_after(train_step)
-    register_model_step.run_after(evaluate_step)
-    steps = [register_model_step]
+    # register_model_step.run_after(evaluate_step)
+    steps = [evaluate_step]
 
     train_pipeline = Pipeline(workspace=aml_workspace, steps=steps)
     train_pipeline.validate()
     published_pipeline = train_pipeline.publish(
-        name="training-pipeline",
-        description="Model training/retraining pipeline"
+        name=pipeline_name,
+        description="Model training/retraining pipeline",
+        version=build_id
     )
-
-    train_pipeline_json = {}
-    train_pipeline_json["rest_endpoint"] = published_pipeline.endpoint
-    json_file_path = "ml_service/pipelines/train_pipeline.json"
-    with open(json_file_path, "w") as outfile:
-        json.dump(train_pipeline_json, outfile)
+    print(f'Published pipeline: {published_pipeline.name}')
+    print(f'for build {published_pipeline.version}')
 
 
 if __name__ == '__main__':
