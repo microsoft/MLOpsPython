cd04bc4de5f5d3e3dc9b5c1c70c5f467fc5dd5b5
Eugene Fedorenko <efedorenko@Eugenes-MBP.guest.corp.microsoft.com>
2019-09-24 16:08:17 -0700

    Play with R

---
+++ code/training/r_train.r
@@ -0,0 +1,49 @@
+# Databricks notebook source
+print(R.version.string)
+
+# COMMAND ----------
+
+mount_name = "kcmunninstoragev2-databricks" # Change this to the mount name in your Databricks workspace
+
+# COMMAND ----------
+
+# Load Training Data
+#path = paste("/dbfs/mnt/",mount_name,"/weight_data.csv",sep="")
+path="../../../data/train/weight_data.csv"
+print(paste("Reading file from",path))
+
+routes<-read.csv(path, header=TRUE)
+
+# The predictor vector (height).
+x <- routes$height
+# The response vector (weight).
+y <- routes$weight
+# Apply the lm() function.
+model <- lm(y~x)
+
+# COMMAND ----------
+
+routes
+
+# COMMAND ----------
+
+# Make Predictions
+df_test_heights <- data.frame(x = as.numeric(c(115,20)))
+result <-  predict(model,df_test_heights)
+print(result)
+
+# COMMAND ----------
+
+# Save the model to blob storage
+#model_path = paste("/dbfs/mnt/",mount_name,"/models/model.rds",sep="")
+model_path="model.rds"
+saveRDS(model, model_path)
+
+# COMMAND ----------
+
+# View model details
+print(model)
+
+# COMMAND ----------
+
+print('Completed')
