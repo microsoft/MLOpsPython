df93cd0d6127109ad4093339bc9a856c4bf5820a
Eugene Fedorenko <efedorenko@Eugenes-MBP.guest.corp.microsoft.com>
2019-09-25 16:10:27 -0700

    play with R

--- code/training/train_with_r.py
+++ code/training/train_with_r.py
@@ -1,7 +1,18 @@
 import os
+import argparse
 
-os.system("Rscript r_train.r")
-os.system("ls -ltr")
+parser = argparse.ArgumentParser("train")
+parser.add_argument(
+    "--AZUREML_SCRIPT_DIRECTORY_NAME",
+    type=str,
+    help="folder",
+)
+
+args = parser.parse_args()
+folder = args.AZUREML_SCRIPT_DIRECTORY_NAME
+
+os.system("Rscript "+folder+"/r_train.r")
+os.system("ls -ltr "+folder)
 os.system("pwd")
 os.system("env")
 os.system("ls -ltr /local_disk0")
@@ -12,3 +23,4 @@ os.system("ls -ltr /databricks/spark/scripts")
 os.system("ls -ltr /databricks/spark/python")
 os.system("whereis train_with_r.py")
 os.system("ls -ltr dbfs/143eb546-b32d-4e42-8194-f43c9d9f9e5f")
+--AZUREML_SCRIPT_DIRECTORY_NAME
