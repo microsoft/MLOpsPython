4abbad0dd492047430ff466740db840813f5a1cc
Eugene Fedorenko <efedorenko@Eugenes-MBP.guest.corp.microsoft.com>
2019-09-26 16:00:51 -0700

    linting

--- code/training/R/train_with_r.py
+++ code/training/R/train_with_r.py
@@ -11,5 +11,5 @@ parser.add_argument(
 args, unknown = parser.parse_known_args()
 folder = args.AZUREML_SCRIPT_DIRECTORY_NAME
 
-os.system("cd " + "/dbfs/" + folder + " && Rscript " + 
+os.system("cd " + "/dbfs/" + folder + " && Rscript " +
           "/dbfs/" + folder + "/r_train.r")
