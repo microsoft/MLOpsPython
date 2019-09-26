182ac1cd035f64a64efee0b5940f4e6bb0bbedaa
Eugene Fedorenko <efedorenko@Eugenes-MBP.guest.corp.microsoft.com>
2019-09-25 17:33:52 -0700

    playing with R

--- code/training/train_with_r.py
+++ code/training/train_with_r.py
@@ -12,16 +12,5 @@ args, unknown = parser.parse_known_args()
 folder = args.AZUREML_SCRIPT_DIRECTORY_NAME
 
 os.system("cd "+"/dbfs/"+folder +" && Rscript "+"/dbfs/"+folder+"/r_train.r")
-os.system("ls -ltr ")
-
-
-# os.system("pwd")
-# os.system("env")
-# os.system("ls -ltr /local_disk0")
-# os.system("ls -ltr /local_disk0/tmp")
-# os.system("ls -ltr /databricks")
-# os.system("ls -ltr /databricks/spark")
-# os.system("ls -ltr /databricks/spark/scripts")
-# os.system("ls -ltr /databricks/spark/python")
 
 
