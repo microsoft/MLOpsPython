d0a1e5b283f84ed4b884db75b781e258584e3021
Eugene Fedorenko <eugene.fedor@gmail.com>
2019-10-15 09:43:12 -0700

    Training a model with R on ML Compute and Databricks (#77)

---
+++ code/training/R/train_with_r.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("Rscript r_train.r && ls -ltr model.rds")
---
+++ ml_service/pipelines/build_train_pipeline_with_r.py
@@ -0,0 +1,78 @@
+from azureml.pipeline.steps import PythonScriptStep
+from azureml.pipeline.core import Pipeline  # , PipelineData
+from azureml.core.runconfig import RunConfiguration, CondaDependencies
+# from azureml.core import Datastore
+import os
+import sys
+from dotenv import load_dotenv
+sys.path.append(os.path.abspath("./ml_service/util"))  # NOQA: E402
+from workspace import get_workspace
+from attach_compute import get_compute
+
+
+def main():
+    load_dotenv()
+    workspace_name = os.environ.get("BASE_NAME")+"-AML-WS"
+    resource_group = os.environ.get("BASE_NAME")+"-AML-RG"
+    subscription_id = os.environ.get("SUBSCRIPTION_ID")
+    tenant_id = os.environ.get("TENANT_ID")
+    app_id = os.environ.get("SP_APP_ID")
+    app_secret = os.environ.get("SP_APP_SECRET")
+    vm_size = os.environ.get("AML_COMPUTE_CLUSTER_CPU_SKU")
+    compute_name = os.environ.get("AML_COMPUTE_CLUSTER_NAME")
+    build_id = os.environ.get("BUILD_BUILDID")
+    pipeline_name = os.environ.get("TRAINING_PIPELINE_NAME")
+
+    # Get Azure machine learning workspace
+    aml_workspace = get_workspace(
+        workspace_name,
+        resource_group,
+        subscription_id,
+        tenant_id,
+        app_id,
+        app_secret)
+    print(aml_workspace)
+
+    # Get Azure machine learning cluster
+    aml_compute = get_compute(
+        aml_workspace,
+        compute_name,
+        vm_size)
+    if aml_compute is not None:
+        print(aml_compute)
+
+    run_config = RunConfiguration(conda_dependencies=CondaDependencies.create(
+        conda_packages=['numpy', 'pandas',
+                        'scikit-learn', 'tensorflow', 'keras'],
+        pip_packages=['azure', 'azureml-core',
+                      'azure-storage',
+                      'azure-storage-blob'])
+    )
+    run_config.environment.docker.enabled = True
+    run_config.environment.docker.base_image = "mcr.microsoft.com/mlops/python"
+
+    train_step = PythonScriptStep(
+        name="Train Model",
+        script_name="train_with_r.py",
+        compute_target=aml_compute,
+        source_directory="code/training/R",
+        runconfig=run_config,
+        allow_reuse=False,
+    )
+    print("Step Train created")
+
+    steps = [train_step]
+
+    train_pipeline = Pipeline(workspace=aml_workspace, steps=steps)
+    train_pipeline.validate()
+    published_pipeline = train_pipeline.publish(
+        name=pipeline_name + "_with_R",
+        description="Model training/retraining pipeline",
+        version=build_id
+    )
+    print(f'Published pipeline: {published_pipeline.name}')
+    print(f'for build {published_pipeline.version}')
+
+
+if __name__ == '__main__':
+    main()
