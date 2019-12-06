fd39e06c41521c7ef83a5808e69af438ea64b6e2
David Tesar <david.tesar@microsoft.com>
2019-12-06 15:21:25 -0800

    fix lint

--- code/scoring/score.py
+++ code/scoring/score.py
@@ -68,4 +68,3 @@ if __name__ == "__main__":
     test_row = '{"data":[[1,2,3,4,5,6,7,8,9,10],[10,9,8,7,6,5,4,3,2,1]]}'
     prediction = run(test_row, {})
     print("Test result: ", prediction)
-    
