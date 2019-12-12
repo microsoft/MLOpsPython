6b811610f182dd3584aa72e79eb6c6da748b5de4
Eugene Fedorenko <efedorenko@amlia-0611.redmond.corp.microsoft.com>
2019-12-12 15:03:04 -0800

    revert back

--- ml_service/util/create_scoring_image.py
+++ ml_service/util/create_scoring_image.py
@@ -25,7 +25,7 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-model = Model(ws, name=e.model_name)
+model = Model(ws, name=e.model_name, version=e.model_version)
 os.chdir("./code/scoring")
 
 image_config = ContainerImage.image_configuration(
