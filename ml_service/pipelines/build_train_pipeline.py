1b262887c87e248eae1203f58c5bbae4ebf59f76
Eugene Fedorenko <efedorenko@Eugenes-MBP.guest.corp.microsoft.com>
2020-01-09 15:39:13 -0800

    linting

--- ml_service/pipelines/build_train_pipeline.py
+++ ml_service/pipelines/build_train_pipeline.py
@@ -58,9 +58,8 @@ def main():
     if (e.datastore_name is not None and e.datafile_name is not None):
         dataset_name = e.dataset_name
         datastore = Datastore.get(aml_workspace, e.datastore_name)
-        dataset = Dataset.Tabular.
-            from_delimited_files(path=[(datastore,
-                                        e.datafile_name)])
+        data_path = [(datastore, e.datafile_name)]
+        dataset = Dataset.Tabular.from_delimited_files(path=data_path)
         dataset.register(workspace=aml_workspace,
                          name=e.dataset_name,
                          description="dataset with training data")
