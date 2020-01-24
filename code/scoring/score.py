f1b9c6ff420ce8e2f79c4667541dacadb9c2f66e
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-01-24 23:15:03 +0100

    Update score.py

--- code/scoring/score.py
+++ code/scoring/score.py
@@ -57,7 +57,6 @@ output_sample = numpy.array([
     3693.645386402646])
 
 
-
 # Inference_schema generates a schema for your web service
 # It then creates an OpenAPI (Swagger) specification for the web service
 # at http://<scoring_base_url>/swagger.json
