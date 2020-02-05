68d2d921104337475d571e46aa1299e5fe8f509b
sushantdivate <sudivate@microsoft.com>
2020-02-04 16:29:27 -0800

    conda activation

--- lint-and-test.sh
+++ lint-and-test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eux
-source activate mlopspython_ci
+conda activate mlopspython_ci
 flake8 --output-file=lint-testresults.xml --format junit-xml
 python -m pytest tests/unit --cov=diabetes_regression --cov-report=html --cov-report=xml --junitxml=unit-testresults.xml
