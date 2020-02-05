93a75fdaf691276e6d68454c26ea608193549beb
sushantdivate <sudivate@microsoft.com>
2020-02-04 17:25:53 -0800

    removed conda activattion

--- lint-and-test.sh
+++ lint-and-test.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -eux
-conda activate mlopspython_ci
 flake8 --output-file=lint-testresults.xml --format junit-xml
 python -m pytest tests/unit --cov=diabetes_regression --cov-report=html --cov-report=xml --junitxml=unit-testresults.xml
