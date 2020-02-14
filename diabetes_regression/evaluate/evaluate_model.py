529c441c6533af8ec8e260184a87b99d920bef1c
Graeme Malcolm <gmalc@microsoft.com>
2020-02-13 22:40:35 -0800

    tidied up code

--- diabetes_regression/evaluate/evaluate_model.py
+++ diabetes_regression/evaluate/evaluate_model.py
@@ -123,9 +123,8 @@ try:
         model_name, tag_name, exp.name, ws)
 
     if (model is not None):
-        if (metric_eval not in model.tags):
-            production_model_mse = 10000
-        else:
+        production_model_mse = 10000
+        if (metric_eval in model.tags):
             production_model_mse = float(model.tags[metric_eval])
         new_model_mse = float(run.parent.get_metrics().get(metric_eval))
         if (production_model_mse is None or new_model_mse is None):
