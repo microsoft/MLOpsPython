8748bdd71122e63849df7970f8339cc852dc954d
jotaylo <jotaylo@microsoft.com>
2020-03-05 08:44:47 -0800

    fix import (#222)
    Fix of importing Dataset, Datastore, Workspace

--- diabetes_regression/training/train_aml.py
+++ diabetes_regression/training/train_aml.py
@@ -23,7 +23,8 @@ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THE SOFTWARE CODE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 """
-from azureml.core.run import Dataset, Datastore, Run, Workspace
+from azureml.core.run import Run
+from azureml.core import Dataset, Datastore, Workspace
 import os
 import argparse
 import joblib
