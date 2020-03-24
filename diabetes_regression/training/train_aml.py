352ebbeba745ae1f421dddc7f36f81c4db766eee
jotaylo <jotaylo@microsoft.com>
2020-03-24 10:48:35 -0700

    Model registration tags come from parameters.json (#237)

--- diabetes_regression/training/train_aml.py
+++ diabetes_regression/training/train_aml.py
@@ -55,7 +55,7 @@ def main():
         "--model_name",
         type=str,
         help="Name of the Model",
-        default="sklearn_regression_model.pkl",
+        default="diabetes_model.pkl",
     )
 
     parser.add_argument(
